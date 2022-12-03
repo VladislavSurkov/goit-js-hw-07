c6f358772976697f0aa8f9d3b5c0527753ad7908
VladislavSurkov <fvhabjrega@gmail.com>
2022-12-03 22:38:20 +0200

    hw7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,39 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js"
 // Change code below this line
+const galleryEl = document.querySelector(".gallery")
 
-console.log(galleryItems);
+const createImages = (options) => options.map(
+    ({ preview, original, description }) =>
+      `<a class="gallery__link">
+      <img class = "gallery__image" src= "${preview}" data-source= "${original}" alt= "${description}"></a>`
+    ).join("")
+
+galleryEl.insertAdjacentHTML("beforeend", createImages(galleryItems))
+
+
+const onGalleryEl = evt => {
+  evt.preventDefault()
+  const imgEl = evt.target
+
+  if (imgEl.nodeName !== "IMG") {
+    return
+  }
+
+
+  const instance = basicLightbox.create(
+    `<img width="1400" height="900" src="${imgEl.dataset.source}">`
+  )
+  instance.show()
+  
+  
+  const onPressKeyESC = (evt) => {
+    if (evt.code === "Escape") {
+      instance.close()
+      window.removeEventListener("keydown", onPressKeyESC)
+    }}
+  if (instance.visible()) {
+    window.addEventListener("keydown", onPressKeyESC)
+  }
+}
+
+galleryEl.addEventListener("click", onGalleryEl);
